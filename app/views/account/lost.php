d78f41348a02ac4dcef54700330c61ebe11d4822
VolodymyrShkykavyi <volodymyr.shkykavyi@gmail.com>
2018-05-08 17:59:47 +0300

    nothing special

--- app/views/account/lost.php
+++ app/views/account/lost.php
@@ -1,12 +1,22 @@
 <?php
 if (!isset($ViewData['result']) && isset($ViewData['token']) &&
-	isset($ViewData['login']) && $ViewData['token'] && $ViewData['login']):?>
-    <form method="post" action="/account/lost">
-        <input type="password" name="newPassword" placeholder="New password">
-        <input type="password" name="repeatPassword" placeholder="Repeat password">
-        <input type="hidden" name="token" value="<?= $ViewData['token']; ?>">
-        <input type="hidden" name="login" value="<?= $ViewData['login']; ?>">
-        <button type="submit" class="btn" name="action" value="resetPassword">Reset password</button>
+    isset($ViewData['login']) && $ViewData['token'] && $ViewData['login']):?>
+    <form method="post" action="/account/lost" class="form-row  mt-3 mb-4">
+        <div class="col-12 mt-1">
+            <input type="password" name="newPassword" placeholder="New password">
+        </div>
+        <div class="col-12 mt-1">
+            <input type="password" name="repeatPassword" placeholder="Repeat password">
+        </div>
+        <div class="col-12 mt-1">
+            <input type="hidden" name="token" value="<?= $ViewData['token']; ?>">
+        </div>
+        <div class="col-12 mt-1">
+            <input type="hidden" name="login" value="<?= $ViewData['login']; ?>">
+        </div>
+        <div class="col-12 mt-1">
+            <button type="submit" class="btn btn-primary" name="action" value="resetPassword">Reset password</button>
+        </div>
     </form>
 <?php elseif (isset($ViewData['result']) && $ViewData['result'] == 'OK'): ?>
     <span class="text-success">Your password has been changed!</span>
@@ -15,6 +25,6 @@ if (!isset($ViewData['result']) && isset($ViewData['token']) &&
 <?php else: ?>
     <form method="post" action="/account/lost">
         <input type="text" name="login" placeholder="Username">
-        <button type="submit" name="action" value="sendMail">Get reinitialization mail</button>
+        <button type="submit" class="btn btn-primary" name="action" value="sendMail">Get reinitialization mail</button>
     </form>
 <?php endif; ?>
